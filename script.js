08b0412906fde6b9acc41bf25fbcdc20090baaf9
Douglas-roy <douglasroy.pro@gmail.com>
2024-07-11 17:37:05 +0200

    Update script.js

--- script.js
+++ script.js
@@ -583,35 +583,35 @@ function placePieces(square,row,col){
         return;
     }
     if(row==6){
-        place(square,'https://cdn.jsdelivr.net/gh/Douglas-roy/Doughledogtest@main/pieces/wPawn.png');
+        place(square,'wPawn.png');
         square.dataset.piece="wP";
     }else if(row==1){
-        place(square,'https://cdn.jsdelivr.net/gh/Douglas-roy/Doughledogtest@main/pieces/bPawn.png');
+        place(square,'bPawn.png');
         square.dataset.piece="bP";
     }else{
         let color=row?"w":"b";
         switch(col){
             case 0:
             case 7://rooks
-                place(square,color+'https://cdn.jsdelivr.net/gh/Douglas-roy/Doughledogtest@main/pieces/Rook.png');
+                place(square,color+'Rook.png');
                 square.dataset.piece=color+"R";
                 break;
             case 1:
             case 6://knights
-                place(square,color+'https://cdn.jsdelivr.net/gh/Douglas-roy/Doughledogtest@main/pieces/Knight.png');
+                place(square,color+'Knight.png');
                 square.dataset.piece=color+"N";
                 break;
             case 2:
             case 5: //bishops
-                place(square,color+'https://cdn.jsdelivr.net/gh/Douglas-roy/Doughledogtest@main/pieces/Bishop.png');
+                place(square,color+'Bishop.png');
                 square.dataset.piece=color+"B";
                 break;
             case 3: //queen
-                place(square,color+'https://cdn.jsdelivr.net/gh/Douglas-roy/Doughledogtest@main/pieces/Queen.png');
+                place(square,color+'Queen.png');
                 square.dataset.piece=color+"Q";
                 break;
             default: //king
-                place(square,color+'https://cdn.jsdelivr.net/gh/Douglas-roy/Doughledogtest@main/pieces/King.png');
+                place(square,color+'King.png');
                 square.dataset.piece=color+"K";
         }
     } 
